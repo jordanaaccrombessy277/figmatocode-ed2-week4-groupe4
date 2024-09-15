08a5ed6d4024041e452c0b276a1226fbe34264ad
Barronakn <barronakn@gmail.com>
2024-09-15 11:08:02 +0100

    "Update DashboardLayout and DashboardUser components with CSS class changes in full screen"

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -72,7 +72,7 @@ function DashboardLayout() {
           </div>
         </aside>
 
-        <main className="max-w-full xl:ml-52 p-3 sm:p-6 -z-40 mt-20">
+        <main className="2xl:absolute 2xl:top-1/2 2xl:left-1/2 2xl:-translate-x-1/2 2xl:-translate-y-1/2 max-w-full xl:ml-52 2xl:ml-32 p-3 sm:p-6 -z-40 mt-20">
           <Outlet />
         </main>
       </div>
--- src/pages/DashbordUser.jsx
+++ src/pages/DashbordUser.jsx
@@ -6,7 +6,6 @@ function DashboardUser() {
     <div className="p-2 sm:p-4 rounded shadow w-full">
       <h2 className="text-xl md:text-2xl font-semibold mb-4">Appointment List</h2>
 
-      {/* Ajout de 'w-full' pour garantir que le conteneur occupe toute la largeur */}
       <div className="w-full overflow-x-auto">
         <UserAppointmentTable />
       </div>
