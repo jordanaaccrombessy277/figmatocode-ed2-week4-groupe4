d83676bf2ac6dbc7699ddf37fcf21ece7d2ae2ee
iovann <iovannatcho@gmail.com>
2024-09-18 00:56:31 +0200

    admindashboard

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -4,7 +4,6 @@ import { Outlet } from 'react-router-dom'
 function DashboardLayout() {
   return (
     <div>
-       dashboard layout
        <Outlet />
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        "gray-primary" : "#6C87AE",
+        "blue-primary" : "#3A8EF6",
+        "purple-primary" : "#6F3AFA",
+      },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to right, #3A8EF6, #6F3AFA)',
+      },
+    },
   },
   plugins: [],
 }
-
